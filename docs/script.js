4f9515b0f882547dbc4be93046595f5aa4a924bb
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-04-19 17:11:32 +0530

    Rework game to work with UI

--- docs/script.js
+++ docs/script.js
@@ -17,7 +17,14 @@ const OUTCOMES = {
 };
 
 const START = "Game Start";
-const EXIT = "Game Over";
+const END = "Game Over";
+
+const choiceButtons = document.querySelector("#choices");
+const scoreDisplay = document.querySelectorAll(".score");
+
+const ROUNDSTOWIN = 5;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
@@ -31,145 +38,35 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Enter Rock, Paper, or Scissors");
-    try {
-        do {
-            playerChoice = playerChoice.toUpperCase();
-            switch(playerChoice) {
-                case CHOICES.Rock:
-                case CHOICES.Paper:
-                case CHOICES.Scissors:
-                    return playerChoice;
-                default:
-                    playerChoice = prompt("Invalid Input! Enter Rock, Paper, or Scissors");
-            }
-            if (playerChoice == null) {
-                throw "cancel";
-            }
-        } while(true)
-    } catch (error) {
-        return EXIT;
-    }
-}
-
-function gameRound(playerChoice = getPlayerChoice(), computerChoice = getComputerChoice()) {
-    if (playerChoice === EXIT) {
-        return EXIT;
-    }
-    
+function getRoundOutcome(playerChoice, computerChoice) {
     let roundOutcome;
+
+    playerChoice = playerChoice.toUpperCase();
     if(playerChoice === computerChoice) {
-        roundOutcome = OUTCOMES.Draw;
+        roundOutcome = OUTCOMES.Draw + playerChoice + " draws " + computerChoice;
     }
     else if(((playerChoice === CHOICES.Rock) && (computerChoice === CHOICES.Paper))
             || ((playerChoice === CHOICES.Paper) && (computerChoice === CHOICES.Scissors))
             || ((playerChoice === CHOICES.Scissors) && (computerChoice === CHOICES.Rock))) {
-        roundOutcome = OUTCOMES.Lose;
+        ++computerScore;
+        roundOutcome = OUTCOMES.Lose + computerChoice + " beats " + playerChoice;
     }
     else {
-        roundOutcome = OUTCOMES.Win;
+        ++playerScore;
+        roundOutcome = OUTCOMES.Win + playerChoice + " beats " + computerChoice;
     }
     return roundOutcome;
 }
 
-function willPlay() {
-    let play;
-    play = prompt("Do you want to play Rock, Paper, Scissors? [Y/N]", "Y")
-    try {
-        do {
-            play = play.toUpperCase();
-            switch(play) {
-                case "Y": return true;
-                case "N": return false;
-                default:
-                    play = prompt("Invalid Input! Enter [Y/N]", "Y");
-            }
-            if(play == null) {
-                throw "cancel";
-            }
-        } while(true)
-    } catch(error) {
-        return false;
-    }
+function playRound(playerChoice, computerChoice = getComputerChoice()) {
+    roundOutcome = getRoundOutcome(playerChoice, computerChoice);
+    currentScore = "CURRENT SCORE:\n YOU: " + playerScore + "\nCOMPUTER: " + computerScore;
+    scoreDisplay[0].textContent = roundOutcome;
+    scoreDisplay[1].textContent = currentScore;
 }
 
-function numberOfRounds() {
-    let rounds = Number(prompt("Enter the number of rounds you want to play:", 5));
-    try {
-        do {
-            if (Number.isNaN(rounds)) {
-                rounds = Number(prompt("Not a Number! Enter the number of rounds you want to play:", 5));
-            }
-            else if (rounds == 0)
-                throw EXIT;
-            else return rounds;
-            
-        } while(true)
-    } catch {
-        return EXIT;
-    }
-}
-
-function printResult(playerScore = 0, computerScore = 0, currentScore = "YOU: 0\nCOMPUTER: 0") {
-
-    if (playerScore > computerScore) {
-        console.log(OUTCOMES.Win);
-        console.log("Final Score:\n" + currentScore);
-    }
-    else if (playerScore < computerScore) {
-        console.log(OUTCOMES.Lose);
-        console.log("Final Score:\n" + currentScore);
-    }
-    else {
-        console.log(OUTCOMES.Draw);
-        console.log("Final Score:\n" + currentScore);
-    }
-    return;
-}
-
-function playGame() {
-    let cancelRound = false;
-    while(cancelRound || willPlay()) {
-        cancelRound = false;
-        let rounds = numberOfRounds();
-        if (rounds != EXIT) {
-            console.log(START);
-
-            let playerScore = 0;
-            let computerScore = 0;
-            let currentScore = "YOU: 0\nCOMPUTER: 0";
-
-            for (i = 0; i < rounds; ++i) {
-                let playerChoice = getPlayerChoice();
-                let computerChoice = getComputerChoice();
-                let roundOutcome = gameRound(playerChoice, computerChoice);
-                switch(roundOutcome) {
-                    case OUTCOMES.Draw:
-                        roundOutcome = OUTCOMES.Draw + playerChoice + " draws " + computerChoice;
-                    break;
-                    case OUTCOMES.Lose:
-                        ++computerScore;
-                        roundOutcome = OUTCOMES.Lose + computerChoice + " beats " + playerChoice;
-                    break;
-                    case OUTCOMES.Win:
-                        ++playerScore;
-                        roundOutcome = OUTCOMES.Win + playerChoice + " beats " + computerChoice;
-                    break;
-                    default:
-                        cancelRound = true;
-                }
-                if (cancelRound)
-                    break;
-                currentScore = "YOU: " + playerScore + "\nCOMPUTER: " + computerScore;
-                console.log(roundOutcome);
-                console.log(currentScore);
-            }
-
-            printResult(playerScore, computerScore, currentScore);
-        }
-    }
-    return EXIT;
-}
-
-console.log(playGame());
+choiceButtons.addEventListener("click", function (e) {
+    const choice = e.target;
+    if (choice.tagName === 'BUTTON')
+        playRound(choice.id);
+});
